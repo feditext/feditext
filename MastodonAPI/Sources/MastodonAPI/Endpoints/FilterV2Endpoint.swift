ea306685886ff4a82ce118a5fc7830ecb4bf1a79
Vyr Cossont <VyrCossont@users.noreply.github.com>
2025-05-08 21:34:01 -0700

    Start adding more filter v2 endpoints
    These aren't used yet but we need to make a new build for TestFlight anyway

---
+++ MastodonAPI/Sources/MastodonAPI/Endpoints/FilterV2Endpoint.swift
@@ -0,0 +1,76 @@
+// Copyright © 2025 Vyr Cossont. All rights reserved.
+
+import Foundation
+import HTTP
+import Mastodon
+
+// TODO: (Vyr) fill out other filters v2 endpoints
+public enum FilterV2Endpoint {
+    case get(filterID: FilterV2.ID)
+
+    case create(
+        title: String,
+        context: [Filter.Context],
+        action: FilterV2.Action,
+        expiresIn: Date?
+    )
+}
+
+extension FilterV2Endpoint: Endpoint {
+    public typealias ResultType = FilterV2
+
+    public var APIVersion: String { "v2" }
+
+    public var context: [String] {
+        defaultContext + ["filters"]
+    }
+
+    public var pathComponentsInContext: [String] {
+        switch self {
+        case .get(let filterID):
+            [filterID]
+
+        case .create:
+            []
+        }
+    }
+
+    public var method: HTTPMethod {
+        switch self {
+        case .get:
+            .get
+
+        case .create:
+            .post
+        }
+    }
+
+    public var jsonBody: [String : Any]? {
+        switch self {
+        case let .create(
+            title,
+            context,
+            action,
+            expiresIn
+        ):
+            var object: [String: Any] = [
+                "title": title,
+                "context": context.map(\.rawValue),
+                "filter_action": action.rawValue,
+            ]
+
+            if let expiresIn {
+                object["expires_in"] = Int(expiresIn.timeIntervalSinceNow)
+            }
+
+            return object
+
+        case .get:
+            return nil
+        }
+    }
+
+    public var requires: APICapabilityRequirements? {
+        FiltersV2Endpoint.filters.requires
+    }
+}
---
+++ MastodonAPI/Sources/MastodonAPI/Endpoints/FilterV2StatusEndpoint.swift
@@ -0,0 +1,44 @@
+// Copyright © 2025 Vyr Cossont. All rights reserved.
+
+import Foundation
+import HTTP
+import Mastodon
+
+public enum FilterV2StatusEndpoint {
+    case add(filterID: FilterV2.ID, statusID: Status.ID)
+}
+
+extension FilterV2StatusEndpoint: Endpoint {
+    public typealias ResultType = FilterV2.Status
+
+    public var APIVersion: String { "v2" }
+
+    public var context: [String] {
+        defaultContext + ["filters"]
+    }
+
+    public var pathComponentsInContext: [String] {
+        switch self {
+        case .add(let filterID, _):
+            [filterID, "statuses"]
+        }
+    }
+
+    public var method: HTTPMethod {
+        switch self {
+        case .add:
+            .post
+        }
+    }
+
+    public var jsonBody: [String: Any]? {
+        switch self {
+        case let .add(_, statusID):
+            ["status_id": statusID]
+        }
+    }
+
+    public var requires: APICapabilityRequirements? {
+        FiltersV2Endpoint.filters.requires
+    }
+}
